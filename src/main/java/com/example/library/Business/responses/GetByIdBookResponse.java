0b70405d6a0647ee6dacec851ea52e6c6bffbf11
Muhammet Ali Colak <m.alicolak01@hotmail.com>
2025-08-18 14:14:20 +0300

    Designed response for GetByIdBookResponse

---
+++ src/main/java/com/example/library/Business/responses/GetByIdBookResponse.java
@@ -0,0 +1,18 @@
+package com.example.library.Business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdBookResponse {
+    private String title;
+    private String author;
+    private String publisher;
+    private short pageCount;
+    private LocalDate publishDate;
+}
