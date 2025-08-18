540a0784cf68e4a89d72e4a6fe6959673f18ac79
Muhammet Ali Colak <m.alicolak01@hotmail.com>
2025-08-18 14:10:45 +0300

    Designed BookService for CRUD endpoints

--- src/main/java/com/example/library/Business/abstracts/BookService.java
+++ src/main/java/com/example/library/Business/abstracts/BookService.java
@@ -1,7 +1,9 @@
 package com.example.library.Business.abstracts;
 
 import com.example.library.Business.requests.CreateBookRequest;
+import com.example.library.Business.requests.UpdateBookRequest;
 import com.example.library.Business.responses.GetAllBooksResponse;
+import com.example.library.Business.responses.GetByIdBookResponse;
 
 import java.util.List;
 
@@ -9,4 +11,7 @@ import java.util.List;
 public interface BookService {
     List<GetAllBooksResponse> getAll();
     void add(CreateBookRequest request);
+    void delete(int id);
+    void update(UpdateBookRequest request);
+    GetByIdBookResponse getById(int id);
 }
